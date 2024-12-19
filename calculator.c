e30d22a435ade7c14c3201512caf30d011b579df
KoushikNagaraja <147733332+KoushikNagaraja@users.noreply.github.com>
2024-12-19 07:33:22 +0530

    Update calculator.c
    expanded one function to different functions and used proper naming conventions

--- calculator.c
+++ calculator.c
@@ -2,80 +2,109 @@
 #include<string.h>
 #include<ctype.h>
 #include<stdlib.h>
-int calculate(int a,int b,char exp)
-{
-    switch(exp)
-    {
-        case '+':return a+b;
-        case '-':return a-b;
-        case '*':return a*b;
-        case '/':if(b==0){printf("division by zero error");exit(0);}return a/b;
-        default:printf("Invalid operator");exit(0);
+
+// Function to calculate based on the operator
+int calculate_Sum(int number1, int number2, char operatorChar) {
+    switch (operatorChar) {
+        case '+': return number1 + number2;
+        case '-': return number1 - number2;
+        case '*': return number1 * number2;
+        case '/':
+            if (number2 == 0) {
+                printf("Invalid expression\n");
+                exit(0);
+            }
+            return number1 / number2;
+        default:
+            printf("Invalid expression\n");
+            exit(0);
     }
 }
-int evaluateexpression(const char *character)
-{
-    int nums[100];
-    char operators[100];
-    int numsindex=0;
-    int operatorindex=0;
-    int i=0;
-    while(character[i]!='\0')
-    {
-        if(isspace(character[i]))
-        {
-            i++;continue;
+
+// Function to extract numbers from the expression
+int check_Numbers(const char *expression, int numbersArray[]) {
+    int index = 0, numberIndex = 0;
+    while (expression[index] != '\0') {
+        if (isspace(expression[index])) {
+            index++;
+            continue;
         }
-        if(isdigit(character[i]))
-        {
-            int value=0;
-            while(isdigit(character[i]))
-            {
-                value=value*10+(character[i]-'0');
-                i++;
+        if (isdigit(expression[index])) {
+            int value = 0;
+            while (isdigit(expression[index])) {
+                value = value * 10 + (expression[index] - '0');
+                index++;
             }
-            nums[numsindex++]=value;
-        }
-        else if(strchr("+-*/",character[i]))
-        {
-            operators[operatorindex++]=character[i];    
-            i++;
-        }
-        else
-        {
-            printf("invalid character in expression");exit(0);
+            numbersArray[numberIndex++] = value;
+        } else {
+            index++;
         }
     }
-    for(int i=0;i<operatorindex;i++)
-    {
-        if(operators[i]=='*'||operators[i]=='/')
-        {
-            nums[i]=calculate(nums[i],nums[i+1],operators[i]);
-            for(int j=i+1;j<numsindex-1;j++)
-            {
-                nums[j]=nums[j+1];
+    return numberIndex;
+}
+
+// Function to extract operators from the expression
+int check_Operators(const char *expression, char operatorsArray[]) {
+    int index = 0, operatorIndex = 0;
+    while (expression[index] != '\0') {
+        if (strchr("+-*/", expression[index])) {
+            operatorsArray[operatorIndex++] = expression[index];
+        }
+        index++;
+    }
+    return operatorIndex;
+}
+
+// Function to process multiplication and division
+int process_Multiplication_Division(int numbersArray[], int numbersCount, char operatorsArray[], int operatorsCount) {
+    int newOperatorsCount = operatorsCount;
+    for (int i = 0; i < newOperatorsCount; i++) {
+        if (operatorsArray[i] == '*' || operatorsArray[i] == '/') {
+            numbersArray[i] = calculate_Sum(numbersArray[i], numbersArray[i + 1], operatorsArray[i]);
+            for (int j = i + 1; j < numbersCount - 1; j++) {
+                numbersArray[j] = numbersArray[j + 1];
             }
-            for(int j=i;j<operatorindex-1;j++)
-            {
-                operators[j]=operators[j+1];
+            for (int j = i; j < newOperatorsCount - 1; j++) {
+                operatorsArray[j] = operatorsArray[j + 1];
             }
-            numsindex--;
-            operatorindex--;
+            numbersCount--;
+            newOperatorsCount--;
             i--;
         }
     }
-    for(int i=0;i<operatorindex;i++)
-    {
-        nums[i+1]=calculate(nums[i],nums[i+1],operators[i]);
-    }
-    return nums[operatorindex];
+    return numbersCount;
 }
-int main()
-{
-    char input[100];
-    printf("Enter the Expression below : \n");
-    fgets(input,sizeof(input),stdin);
-    int result=evaluateexpression(input);
-    printf("%d",result);
+
+// Function to process addition and subtraction
+int process_Addition_Subtraction(int numbersArray[], int numbersCount, char operatorsArray[], int operatorsCount) {
+    for (int i = 0; i < operatorsCount; i++) {
+        numbersArray[i + 1] = calculate_Sum(numbersArray[i], numbersArray[i + 1], operatorsArray[i]);
+    }
+    return numbersArray[operatorsCount];
+}
+
+// Function to evaluate the expression
+int evaluate_Expression(const char *expression) {
+     for (int i = 0; expression[i] != '\0'; i++) {
+        if (isalpha(expression[i])) {
+            printf("Invalid expression\n");
+            exit(0);
+        }
+    }
+    int numbersArray[100];
+    char operatorsArray[100];
+    int numbersCount = check_Numbers(expression, numbersArray);
+    int operatorsCount = check_Operators(expression, operatorsArray);
+    numbersCount = process_Multiplication_Division(numbersArray, numbersCount, operatorsArray, operatorsCount);
+    return process_Addition_Subtraction(numbersArray, numbersCount, operatorsArray, operatorsCount - (operatorsCount - numbersCount + 1));
+}
+
+int main() {
+    char userInput[100];
+    printf("Enter the Expression below:\n");
+    fgets(userInput, sizeof(userInput), stdin);
+
+    int result = evaluate_Expression(userInput);
+    printf("The result is %d\n", result);
     return 0;
 }
